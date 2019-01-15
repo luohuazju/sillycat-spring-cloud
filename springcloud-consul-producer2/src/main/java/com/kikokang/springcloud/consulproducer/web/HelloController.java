37646283208b24011e4ce5321dba43a086a3207a
Hua Luo <hluo@lifesize.com>
2019-01-15 14:34:34 -0600

    add health check

--- springcloud-consul-producer2/src/main/java/com/kikokang/springcloud/consulproducer/web/HelloController.java
+++ springcloud-consul-producer2/src/main/java/com/kikokang/springcloud/consulproducer/web/HelloController.java
@@ -1,10 +1,16 @@
 package com.kikokang.springcloud.consulproducer.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	@GetMapping("/health")
+    public String health(){
+        return "hello consul";
+    }
 
 	@RequestMapping("/hello")
     public String hello() {
