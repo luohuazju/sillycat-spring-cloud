9a3e6a4855840b0e436c8c4f1a303823e4e524d6
Hua Luo <hluo@lifesize.com>
2019-06-01 22:06:21 -0500

    add swagger support

--- webflux-with-latency-netty/src/main/java/com/sillycat/webfluxlatency/web/HelloController.java
+++ webflux-with-latency-netty/src/main/java/com/sillycat/webfluxlatency/web/HelloController.java
@@ -6,12 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+@Api(value = "/hello/")
 @RestController
 public class HelloController {
 
+	@ApiOperation(value = "hello latency", response = String.class)
 	@GetMapping("/hello/{latency}")
 	public Mono<String> hello(@PathVariable int latency) {
 		return Mono.just("Welcome to reactive world " + latency).delayElement(Duration.ofMillis(latency));
