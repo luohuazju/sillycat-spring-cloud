4797bd8aea4d36c29eeb04ed695cf0dc035663c5
Hua Luo <hluo@lifesize.com>
2019-06-08 23:03:26 -0500

    use reactor all the way to the botton

---
+++ webflux-with-latency-netty/src/main/java/com/sillycat/webfluxlatency/web/JobCountHistoryController.java
@@ -0,0 +1,29 @@
+package com.sillycat.webfluxlatency.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sillycat.webfluxlatency.domain.JobCountHistory;
+import com.sillycat.webfluxlatency.repository.JobCountHistoryRepositoryCassandra;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import reactor.core.publisher.Flux;
+
+@Api(value = "/jobcounthistory/")
+@RestController
+public class JobCountHistoryController {
+
+	@ApiOperation(value = "fetch job by sourceID", response = JobCountHistory.class)
+	@GetMapping("/jobcounthistory/{sourceID}")
+	public Flux<JobCountHistory> findBySourceID(@PathVariable String sourceID) {
+		Flux<JobCountHistory> results = jobCountHistoryRepositoryCassandra.findBySourceID(sourceID);
+		return results;
+	}
+
+	@Autowired
+	JobCountHistoryRepositoryCassandra jobCountHistoryRepositoryCassandra;
+
+}
