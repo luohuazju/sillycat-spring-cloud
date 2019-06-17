a9bae60d8da3c3fd55c9f623c0262c244698ac37
Hua Luo <hluo@lifesize.com>
2019-06-17 14:56:16 -0500

    disable cassandra for now

--- webflux-with-latency-netty/src/main/java/com/sillycat/webfluxlatency/domain/JobCountHistory.java
+++ webflux-with-latency-netty/src/main/java/com/sillycat/webfluxlatency/domain/JobCountHistory.java
@@ -1,39 +1,39 @@
-package com.sillycat.webfluxlatency.domain;
-
-import java.util.Date;
-
-import org.springframework.data.cassandra.core.cql.Ordering;
-import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
-import org.springframework.data.cassandra.core.mapping.Column;
-import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
-import org.springframework.data.cassandra.core.mapping.Table;
-
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Setter
-@Getter
-@ToString
-@Table("jobcounthistory")
-public class JobCountHistory {
-
-	@PrimaryKeyColumn(name = "source_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
-	private String sourceID;
-
-	@PrimaryKeyColumn(name = "record_date", ordinal = 1, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.DESCENDING)
-	private Date recordDate;
-
-	@Column("new_active_count")
-	private Integer newActiveCount;
-
-	@Column("new_admin_count")
-	private Integer newAdminCount;
-
-	@Column("old_active_count")
-	private Integer oldActiveCount;
-
-	@Column("old_admin_count")
-	private Integer oldAdminCount;
-
-}
+//package com.sillycat.webfluxlatency.domain;
+//
+//import java.util.Date;
+//
+//import org.springframework.data.cassandra.core.cql.Ordering;
+//import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+//import org.springframework.data.cassandra.core.mapping.Column;
+//import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+//import org.springframework.data.cassandra.core.mapping.Table;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@Setter
+//@Getter
+//@ToString
+//@Table("jobcounthistory")
+//public class JobCountHistory {
+//
+//	@PrimaryKeyColumn(name = "source_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+//	private String sourceID;
+//
+//	@PrimaryKeyColumn(name = "record_date", ordinal = 1, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.DESCENDING)
+//	private Date recordDate;
+//
+//	@Column("new_active_count")
+//	private Integer newActiveCount;
+//
+//	@Column("new_admin_count")
+//	private Integer newAdminCount;
+//
+//	@Column("old_active_count")
+//	private Integer oldActiveCount;
+//
+//	@Column("old_admin_count")
+//	private Integer oldAdminCount;
+//
+//}
