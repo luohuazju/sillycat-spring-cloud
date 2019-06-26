0cca899d21911e7ec5c1756b9c5705430368ccbb
Hua Luo <hluo@lifesize.com>
2019-06-25 22:29:51 -0500

    disable the redis config

--- mvc-with-latency/src/main/java/com/sillycat/mvclatency/config/RedisClusterConfig.java
+++ mvc-with-latency/src/main/java/com/sillycat/mvclatency/config/RedisClusterConfig.java
@@ -1,36 +1,36 @@
-package com.sillycat.mvclatency.config;
-
-import java.util.List;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
-@Configuration
-@EnableConfigurationProperties
-@ConfigurationProperties("spring.redis.cluster")
-public class RedisClusterConfig {
-	
-	/*
-     * spring.redis.cluster.nodes[0] = 127.0.0.1:7379
-     * spring.redis.cluster.nodes[1] = 127.0.0.1:7380
-     * ...
-     */
-    List<String> nodes;
-    
-	@Bean
-    public RedisConnectionFactory clusterConnectionFactory() {
-        return new JedisConnectionFactory(
-            new RedisClusterConfiguration(this.getNodes()));
-    }
-
-}
+//package com.sillycat.mvclatency.config;
+//
+//import java.util.List;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisClusterConfiguration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Setter
+//@Getter
+//@Configuration
+//@EnableConfigurationProperties
+//@ConfigurationProperties("spring.redis.cluster")
+//public class RedisClusterConfig {
+//	
+//	/*
+//     * spring.redis.cluster.nodes[0] = 127.0.0.1:7379
+//     * spring.redis.cluster.nodes[1] = 127.0.0.1:7380
+//     * ...
+//     */
+//    List<String> nodes;
+//    
+//	@Bean
+//    public RedisConnectionFactory clusterConnectionFactory() {
+//        return new JedisConnectionFactory(
+//            new RedisClusterConfiguration(this.getNodes()));
+//    }
+//
+//}
