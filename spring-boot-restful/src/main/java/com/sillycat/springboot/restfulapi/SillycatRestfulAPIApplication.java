9fca536bfe388a53dc19b19948fef525f117b155
Hua Luo <luohuazju@gmail.com>
2021-06-02 01:19:45 -0500

    springboot and swagger

--- spring-boot-restful/src/main/java/com/sillycat/springboot/restfulapi/SillycatRestfulAPIApplication.java
+++ spring-boot-restful/src/main/java/com/sillycat/springboot/restfulapi/SillycatRestfulAPIApplication.java
@@ -1,5 +1,24 @@
 package com.sillycat.springboot.restfulapi;
 
-public class SillycatRestfulAPIApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class SillycatRestfulAPIApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) throws Exception {
+		log.info("NetsuiteConnectorApplication init! ");
+		SpringApplication.run(SillycatRestfulAPIApplication.class);
+		log.info("NetsuiteConnectorApplication started! ");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SillycatRestfulAPIApplication.class);
+	}
 
 }
