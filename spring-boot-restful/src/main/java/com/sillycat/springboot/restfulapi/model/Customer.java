8208627e73582ffbba1a18c25e35e34fa9339053
Hua Luo <hua.luo@liveramp.com>
2021-09-07 20:05:31 -0500

    add query and native query

--- spring-boot-restful/src/main/java/com/sillycat/springboot/restfulapi/model/Customer.java
+++ spring-boot-restful/src/main/java/com/sillycat/springboot/restfulapi/model/Customer.java
@@ -8,6 +8,10 @@ import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import org.springframework.data.annotation.CreatedDate;
@@ -26,25 +30,29 @@ import lombok.NoArgsConstructor;
 @Data
 @Table(name = "customer")
 @EntityListeners(AuditingEntityListener.class)
+@NamedNativeQueries(value = {
+		@NamedNativeQuery(name = "find_customer_by_first_name_and_last_name", query = "select id, first_name, last_name, email, created_time, updated_time from customer where first_name = ?1 and last_name = ?2", resultClass = Customer.class) })
+@NamedQueries(value = {
+		@NamedQuery(name = "find_customer_by_first_name", query = "select c from Customer c where c.firstName = ?1") })
 public class Customer {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(name = "first_name")
 	private String firstName;
-	
+
 	@Column(name = "last_name")
 	private String lastName;
-	
+
 	private String email;
 
 	@Column(updatable = false, nullable = false, name = "created_time")
-    @CreatedDate
-    private LocalDateTime createdTime;
-	
+	@CreatedDate
+	private LocalDateTime createdTime;
+
 	@Column(nullable = false, name = "updated_time")
-    @LastModifiedDate
-    private LocalDateTime updatedTime;
+	@LastModifiedDate
+	private LocalDateTime updatedTime;
 }
--- spring-boot-restful/src/test/java/com/sillycat/springboot/restfulapi/repository/CustomerRepositoryTest.java
+++ spring-boot-restful/src/test/java/com/sillycat/springboot/restfulapi/repository/CustomerRepositoryTest.java
@@ -24,18 +24,47 @@ public class CustomerRepositoryTest {
 		log.info("testBeanNotNull invoke------");
 		Assert.notNull(customerRepository, "customerRepository can not be null during unit testing");
 	}
-	
+
 	@Test
 	public void testLoadAll() {
 		List<Customer> items = customerRepository.findAll();
 		Assert.isTrue(items.size() > 0, "load all not empty, we have default Hua Luo there");
 	}
-	
+
 	@Test
-	public void testGetById() {
+	public void testFindById() {
 		Optional<Customer> itemOptional = customerRepository.findById(Long.valueOf(2));
 		log.info("getById invoke get item: " + itemOptional.get().toString());
-		Assert.notNull(itemOptional.get(), "Default value should be there");;
+		Assert.notNull(itemOptional.get(), "Default value should be there");
+		;
+	}
+
+	@Test
+	public void testFindByFirstName() {
+		List<Customer> items = customerRepository.findByFirstName("Hua");
+		Assert.notNull(items, "should have firstname Hua");
+		log.info("find hua: " + items.get(0).toString());
+	}
+
+	@Test
+	public void testFindByFirstNameAndLastName() {
+		List<Customer> items = customerRepository.findByFirstNameAndLastName("Hua", "Luo");
+		Assert.notNull(items, "should have firstname Hua lastname Luo");
+		log.info("find hua luo: " + items.get(0).toString());
+	}
+
+	@Test
+	public void testGetCustomerByFirstNameAndLastName() {
+		List<Customer> items = customerRepository.findCustomerByFirstNameAndLastName("Hua", "Luo");
+		Assert.notNull(items, "should have firstname Hua lastname Luo");
+		log.info("find hua luo in query: " + items.get(0).toString());
+	}
+
+	@Test
+	public void testGetCustomerByFirstName() {
+		List<Customer> items = customerRepository.findCustomerByFirstName("Hua");
+		Assert.notNull(items, "should have firstname Hua lastname Luo");
+		log.info("find hua luo in query: " + items.get(0).toString());
 	}
 
 }
