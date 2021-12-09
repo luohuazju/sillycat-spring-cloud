65690a140f0d7b3392ffe3ae52961ea0ebc73bd2
Hua Luo <luohuazju@gmail.com>
2021-12-09 17:00:17 -0600

    commit the latest code

--- spring-boot-restful/src/main/java/com/sillycat/springboot/restfulapi/SillycatRestfulAPIApplication.java
+++ spring-boot-restful/src/main/java/com/sillycat/springboot/restfulapi/SillycatRestfulAPIApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.sillycat.springboot.restfulapi.akka.base.SpringExtension;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
@@ -12,8 +18,15 @@ public class SillycatRestfulAPIApplication extends SpringBootServletInitializer 
 
 	public static void main(String[] args) throws Exception {
 		log.info("NetsuiteConnectorApplication init! ");
-		SpringApplication.run(SillycatRestfulAPIApplication.class);
+		ConfigurableApplicationContext context = SpringApplication.run(SillycatRestfulAPIApplication.class);
 		log.info("NetsuiteConnectorApplication started! ");
+		log.info("Start to init the AKKA system...");
+		SpringExtension ext = context.getBean(SpringExtension.class);
+		ActorSystem system = context.getBean(ActorSystem.class);
+		ActorRef supervisor = system.actorOf(ext.props("outOfBudgetSupervisor").withMailbox("akka.priority-mailbox"),
+				"outOfBudgetSupervisor");
+		log.info("supervisor init with path {}", supervisor.path());
+		log.info("AKKA system success inited...");
 	}
 
 	@Override
---
+++ spring-boot-restful/src/main/java/com/sillycat/springboot/restfulapi/akka/base/SpringExtension.java
@@ -0,0 +1,22 @@
+package com.sillycat.springboot.restfulapi.akka.base;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import akka.actor.Extension;
+import akka.actor.Props;
+
+@Component
+public class SpringExtension implements Extension {
+
+	private ApplicationContext applicationContext;
+
+	public void initialize(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
+
+	public Props props(String actorBeanName) {
+		return Props.create(SpringActorProducer.class, applicationContext, actorBeanName);
+	}
+
+}
