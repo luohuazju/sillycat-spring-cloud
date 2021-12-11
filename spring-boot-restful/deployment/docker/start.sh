825110aff0cb072dc6699680545d681d5d16958f
Hua Luo <luohuazju@gmail.com>
2021-12-10 23:35:38 -0600

    add the build

--- spring-boot-restful/deployment/docker/start.sh
+++ spring-boot-restful/deployment/docker/start.sh
@@ -1,6 +1,8 @@
-#!/bin/sh -ex
+#!/bin/sh 
 
-APPLICATION_SECRET="nosessionshere"
-
-cd /app/spring-boot-restful-*
-bin/start-console.sh
+java -Djava.net.preferIPv4Stack=true \
+	 -XX:MaxMetaspaceSize=256M -XX:+HeapDumpOnOutOfMemoryError \
+	 -XX:HeapDumpPath="/tmp/dump_oom.hprof" \
+	 -Dspring.config.location=file:./conf/application.yaml \
+	 -Dlogging.config=file:./conf/log4j2.xml \
+	 -jar ./lib/spring-boot-restful-*.jar
