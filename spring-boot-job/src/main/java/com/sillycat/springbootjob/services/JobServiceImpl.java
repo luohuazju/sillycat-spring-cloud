52f10310704d99428b01d8e118bcfe83d4ee492a
Hua Luo <hua.luo@liveramp.com>
2022-03-08 16:34:09 -0600

    in memory solution is working

--- spring-boot-job/src/main/java/com/sillycat/springbootjob/services/JobServiceImpl.java
+++ spring-boot-job/src/main/java/com/sillycat/springbootjob/services/JobServiceImpl.java
@@ -1,6 +1,7 @@
 package com.sillycat.springbootjob.services;
 
 import org.jobrunr.jobs.annotations.Job;
+import org.jobrunr.spring.annotations.Recurring;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,12 @@ public class JobServiceImpl implements JobService{
 	public String echo(String message) {
 		log.info("carl is calling this " + message);
 		return "hello, " + message;
+	}
+	
+	@Recurring(id = "JobID-EveryJob", cron = "*/2 * * * *")
+	@Job(name= "EveryJob", retries = 1)
+	public void everyMinumte() {
+		log.info("carl is coding all the time");
 	}
 	
 }
--- spring-boot-job/src/main/java/com/sillycat/springbootjob/web/JobController.java
+++ spring-boot-job/src/main/java/com/sillycat/springbootjob/web/JobController.java
@@ -1,6 +1,9 @@
 package com.sillycat.springbootjob.web;
 
+import java.time.LocalDateTime;
+
 import org.jobrunr.scheduling.JobScheduler;
+import org.jobrunr.scheduling.cron.Cron;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.http.HttpStatus;
@@ -13,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sillycat.springbootjob.services.JobService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/job")
 public class JobController {
@@ -26,6 +32,14 @@ public class JobController {
 	@GetMapping(value = "/enqueue/{input}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> enqueue(@PathVariable("input") @DefaultValue("default") String input) {
 		jobScheduler.enqueue(() -> jobService.echo(input)); //fire and forget
+		String scheduleInput = input + " after 5 minutes";
+		LocalDateTime min1 = LocalDateTime.now().plusMinutes(1);
+		log.info(min1.toString());
+		jobScheduler.schedule(min1, () -> jobService.echo(scheduleInput));
+		// after 5 minutes
+		String everyInput = input + " every 10 minutes";
+		jobScheduler.scheduleRecurrently("JobId-" + input,Cron.every5minutes(), () -> jobService.echo(everyInput));
+		// every 10 minutes
 		return okResponse("job enqueued successfully");
 	}
 
