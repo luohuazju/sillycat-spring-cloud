a019f57872e9311018c4f63d02ef0a5328b6f18f
Hua Luo <luohuazju@gmail.com>
2022-03-11 11:12:06 -0600

    it is using the database, and it works well

--- spring-boot-job/src/main/java/com/sillycat/springbootjob/SpringBootJobApp.java
+++ spring-boot-job/src/main/java/com/sillycat/springbootjob/SpringBootJobApp.java
@@ -4,16 +4,14 @@ import org.jobrunr.scheduling.JobScheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBootJobApp {
-	
-	@Autowired
-	private JobScheduler jobScheduler;
 
-	
 	public static void main(String[] args) {
-        SpringApplication.run(SpringBootJobApp.class, args);
-    }
+		SpringApplication.run(SpringBootJobApp.class, args);
+	}
 
 }
--- spring-boot-job/src/main/java/com/sillycat/springbootjob/config/StorageProviderConfig.java
+++ spring-boot-job/src/main/java/com/sillycat/springbootjob/config/StorageProviderConfig.java
@@ -1,9 +1,15 @@
 package com.sillycat.springbootjob.config;
 
+import javax.sql.DataSource;
+
 import org.jobrunr.jobs.mappers.JobMapper;
 import org.jobrunr.server.JobActivator;
-import org.jobrunr.storage.InMemoryStorageProvider;
 import org.jobrunr.storage.StorageProvider;
+import org.jobrunr.storage.StorageProviderUtils.DatabaseOptions;
+import org.jobrunr.storage.sql.SqlStorageProvider;
+import org.jobrunr.storage.sql.common.DefaultSqlStorageProvider;
+import org.jobrunr.storage.sql.common.db.dialect.AnsiDialect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,14 +17,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StorageProviderConfig {
 
+	@Autowired
+	DataSource dataSource;
 
 	@Bean
 	public StorageProvider storageProvider(JobMapper jobMapper) {
-		InMemoryStorageProvider storageProvider = new InMemoryStorageProvider();
+		// InMemoryStorageProvider storageProvider = new InMemoryStorageProvider();
+		SqlStorageProvider storageProvider = new DefaultSqlStorageProvider(dataSource, new AnsiDialect(), "jobrunr_",
+				DatabaseOptions.SKIP_CREATE);
 		storageProvider.setJobMapper(jobMapper);
 		return storageProvider;
 	}
-	
+
 	@Bean
 	public JobActivator jobActivator(ApplicationContext applicationContext) {
 		return applicationContext::getBean;
