a270b77f2c7b1b53c0281e55e470eefccdd0f9be
Hua Luo <luohuazju@gmail.com>
2022-03-11 11:30:48 -0600

    remove the unused jobs

--- spring-boot-job/src/main/java/com/sillycat/springbootjob/SpringBootJobApp.java
+++ spring-boot-job/src/main/java/com/sillycat/springbootjob/SpringBootJobApp.java
@@ -1,7 +1,5 @@
 package com.sillycat.springbootjob;
 
-import org.jobrunr.scheduling.JobScheduler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
