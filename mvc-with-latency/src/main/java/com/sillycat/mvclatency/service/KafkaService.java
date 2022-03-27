d99dde0183a3e0eedf0ad4c76695fde1c9950dc2
Hua Luo <luohuazju@gmail.com>
2022-03-26 20:35:02 -0500

    disable the kafka testing

--- mvc-with-latency/src/main/java/com/sillycat/mvclatency/service/KafkaService.java
+++ mvc-with-latency/src/main/java/com/sillycat/mvclatency/service/KafkaService.java
@@ -1,7 +1,7 @@
-package com.sillycat.mvclatency.service;
-
-public interface KafkaService {
-
-	public void send(String payload);
-
-}
+//package com.sillycat.mvclatency.service;
+//
+//public interface KafkaService {
+//
+//	public void send(String payload);
+//
+//}
