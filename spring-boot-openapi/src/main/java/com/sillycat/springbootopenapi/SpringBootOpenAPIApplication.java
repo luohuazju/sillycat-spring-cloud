5a7298c2fd67a380792db328e970ae8d055bc281
Hua Luo <luohuazju@gmail.com>
2022-04-04 22:20:58 -0500

    add the CORS configuration

--- spring-boot-openapi/src/main/java/com/sillycat/springbootopenapi/SpringBootOpenAPIApplication.java
+++ spring-boot-openapi/src/main/java/com/sillycat/springbootopenapi/SpringBootOpenAPIApplication.java
@@ -2,6 +2,9 @@ package com.sillycat.springbootopenapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,6 +16,17 @@ public class SpringBootOpenAPIApplication {
 		log.info("SpringBootOpenAPIApplication init! ");
 		SpringApplication.run(SpringBootOpenAPIApplication.class, args);
 		log.info("SpringBootOpenAPIApplication started! ");
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/v1/api-docs").allowedOrigins("*");
+				registry.addMapping("/books/*").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "OPTIONS");
+			}
+		};
 	}
 
 }
