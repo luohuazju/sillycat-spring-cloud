e2413d96007f64a29a29e1766cfbb45c171b5cd0
Carl <luohuazju@gmail.com>
2023-07-30 16:29:01 -0500

    clean up the codes

--- spring-boot-api/src/main/java/com/sillycat/springbootapi/SpringBootAPIApplication.java
+++ spring-boot-api/src/main/java/com/sillycat/springbootapi/SpringBootAPIApplication.java
@@ -2,9 +2,8 @@ package com.sillycat.springbootapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
 public class SpringBootAPIApplication {
 	
--- spring-boot-api/src/main/java/com/sillycat/springbootapi/controller/ConfigClientController.java
+++ spring-boot-api/src/main/java/com/sillycat/springbootapi/controller/ConfigClientController.java
@@ -1,12 +1,7 @@
 package com.sillycat.springbootapi.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.sillycat.springbootapi.config.ConfigBeanAuthor;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,24 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ConfigClientController {
 	
-	
-	@Value("${config.info}")
-    private String configInfo;
-	
-	@Autowired
-	private ConfigBeanAuthor configBeanAuthor;
-
-    @GetMapping("/configInfo")
-    public String getConfigInfo() {
-        return configInfo;
-    }
-    
-    @GetMapping("/author")
-    public ConfigBeanAuthor getAuthor() {
-    	log.info("getCustomer method invoke---------");
-    	return configBeanAuthor;
-    }
-    
     
 
 }
